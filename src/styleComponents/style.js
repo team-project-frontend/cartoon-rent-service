20cbe753a3b92964f94126923da228d314c546b6
kmjang <rudals782@nate.com>
2023-04-06 18:08:58 +0900

    maincommit

---
+++ src/styleComponents/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  min-height: 100vh;
+  font-weight: 100;
+  text-align: center;
+  background-color: #fff;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+`;
