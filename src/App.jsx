c31c544a5ffe484d4c7addd9df47700dc7061411
kmjang <rudals782@nate.com>
2023-04-07 16:23:56 +0900

    경민/feat:라우터구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Client from "./Client";
 import "./index.css";
 import Error from "./components/Error";
 import Login from "./pages/Login";
 import Join from "./pages/Join";
-import Test from "./pages/Test";
-import Main from "./pages/Main";
 
 function App() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
 
   return (
     <>
