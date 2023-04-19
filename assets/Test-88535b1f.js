85f8cfdf50e280d5f635a765b19aac18200fc5b9
kmjang <rudals782@nate.com>
2023-04-19 20:05:05 +0900

    Updates

---
+++ assets/Test-88535b1f.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index-6e7adb6a.js";const e=()=>r("div",{children:"테스트"});export{e as default};
--- assets/Test-9cad777b.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-5e38652f.js";const e=()=>r("div",{children:"테스트"});export{e as default};
