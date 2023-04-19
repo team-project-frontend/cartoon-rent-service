fd30d8d05f3bf71f80010666611ff0c80880403e
kmjang <rudals782@nate.com>
2023-04-19 20:00:04 +0900

    Updates

---
+++ assets/Test-619927cc.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index-49aaea4a.js";const e=()=>r("div",{children:"테스트"});export{e as default};
--- assets/Test-c01bdab6.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-344095ec.js";const e=()=>r("div",{children:"테스트"});export{e as default};
