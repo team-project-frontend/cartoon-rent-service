682e93ef4aa2992c20c70484ab9c6207c1fa78be
kmjang <rudals782@nate.com>
2023-04-21 19:24:22 +0900

    경민/feat/메인 섹션 구분

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import Nav from "../layout/Nav";
+import Reservation from "../components/mainComponent/Reservation";
 
 const Main = () => {
   return (
     <>
-      <Nav />
+      <div className="mainWrap">
+        <div
+          style={{
+            width: "100%",
+            height: "500px",
+            background: " #ddd",
+            textAlign: "center",
+            lineHeight: "250px",
+            fontSize: "36px",
+            borderRadius: "0px 0px 150px 0px",
+          }}
+        >
+          배너영역
+        </div>
+        {/* 영역분리 */}
+        <Reservation />
+        {/* 작업하실 컴포넌트 여기에 추가해주세여ㅕ */}
+      </div>
     </>
   );
 };
