4823a67c950f8e2f390968c4af9b33d90af908c6
park-jian <masichyun77@hanmail.net>
2023-05-03 20:43:12 +0900

    지안/feat/signup이메일 중복체크, 이메일 인증번호 추가

--- src/styleComponents/MainStyle.js
+++ src/styleComponents/MainStyle.js
@@ -18,3 +18,114 @@ export const ReservationContainer = styled.div`
     height: 525px;
   }
 `;
+
+// 메인컴포넌트의 favorite영역
+export const FavoriteWrapper = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  margin-top: 100px;
+`;
+
+export const FavoriteTopArea = styled.div`
+  background-color: #169EF9;
+  width: 100%;
+  height: 466px;
+  border-radius: 150px 0px;
+  color: white;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const FavoriteList = styled.div`
+  width: 100%;
+  height: 50%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const FavoriteContentWrapper = styled.div`
+  margin: 0 auto;
+  margin-top: 100px;
+  max-width: 1440px;
+  padding: 50px 20px;
+  height: 860px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const FavoriteContentSideArea = styled.div`
+  flex-basis: 360px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FavoriteContentSideTitle = styled.div`
+  font-size: 40px;
+  padding: 20px 0 0 20px;
+  height: 262px;
+`;
+
+export const FavoriteContentSideButtonList = styled.div`
+  display: flex;
+  margin: 0 0 20px 20px;
+  height: 100%;
+  flex-wrap: wrap;
+  align-content: space-between;
+  & > button {
+    width: 176px;
+    height: 69px;
+    border-radius: 15px;
+    font-size: 29px;
+  }
+`;
+
+export const FavoriteBottomArea = styled.div`
+  background-color: #F2F9FF;
+  height: 798px;
+`;
+
+export const FavoriteBottomContent = styled.div`
+  height: 398px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  & > h1 {
+    font-size: 40px;
+    font-weight: 700;
+    font-style: normal
+  }
+  & > h1 > select {
+    color: #169EF9;
+    border: none;
+    outline: 0;
+    background-color: #F2F9FF;
+    font-size: 40px;
+    font-weight: 700;
+    text-align: right;
+  }
+`;
+
+export const FavoriteBottomKeywordArea = styled.div`
+  margin-top: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > button {
+    border: 2px solid #169EF9;
+    border-radius: 15px;
+    width: 213px;
+    height: 46px;
+  }
+  & > button > h3 {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 21px;
+    text-align: center;
+  }
+`;
