ab629223585575c5e39aa30af7a276d78e0ec0b7
kmjang <rudals782@nate.com>
2023-05-03 22:30:38 +0900

    .

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Reservation from "../components/mainComponent/Reservation";
 import ShareTaste from "../components/mainComponent/shareTaste/ShareTaste";
+import FavoriteContent from "../components/mainComponent/favorite/FavoriteContent";
+import FavoriteContents from "../components/mainComponent/favorite/FavoriteContents";
 const Main = () => {
   return (
     <>
