45e48f0f573909047e3558aba85f4caf8412b7a6
HaEun <rjadmszhd777@naver.com>
2023-05-03 18:41:52 +0900

    하은/feat/오타수정

--- src/utiles/routes.jsx
+++ src/utiles/routes.jsx
@@ -9,7 +9,7 @@ const Test = loadable(() => import("../pages/Test"), {
 const FIndID = loadable(() => import("../pages/FindID"), {
   fallback: <div>'로딩중.............'</div>, // 여기에 로딩 컴포넌트 넣으시면됩니다
 });
-const FindPassword = loadable(() => import("../pages/FindPassword"), {
+const FIndPassword = loadable(() => import("../pages/FindPassword"), {
   fallback: <div>'로딩중.............'</div>, // 여기에 로딩 컴포넌트 넣으시면됩니다
 });
 
@@ -17,7 +17,7 @@ const routes = [
   { id: 0, path: "/", title: "MainPage", item: <Main /> },
   { id: 1, path: "/test", title: "MainPage", item: <Test /> },
   { id: 2, path: "/findID", title: "FindID", item: <FIndID /> },
-  { id: 3, path: "/findPassword", title: "FindID", item: <FIndPaddword /> },
+  { id: 3, path: "/findPassword", title: "FindPassword", item: <FIndPassword /> },
 
 
 ];
