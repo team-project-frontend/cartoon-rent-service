1f036526c9fec620ae07a621b9b10a03db4b227f
kmjang <rudals782@nate.com>
2023-05-17 19:18:38 +0900

    kmjang/feat/카카오로그인

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Client from "./Client";
 import "./index.css";
@@ -7,16 +7,55 @@ import Login from "./pages/Login";
 import Join from "./pages/Join";
 import SignUp from "./pages/SignUp";
 import { Toast } from "./utiles/toast";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import userState from "./store/userState";
-
+import { useCookies } from "react-cookie";
 function App() {
-  const [isLogin, setIsLogin] = useState(false);
+  const [cookie] = useCookies(["refresh_token"]);
   const globalValue = useRecoilValue(userState);
+  const loginState = useSetRecoilState(userState); // 값만 변경 시키기
+
   useEffect(() => {
     console.log(globalValue, "globalValue");
-    console.log(globalValue.isLogin);
   }, [globalValue]);
+
+  useEffect(() => {
+    if (window.Kakao) {
+      window.Kakao.init("b49e4e1e3fb0885c87c93cdfa9f309c3");
+      console.log(window.Kakao, "카카오 sdk 확인");
+      getKakaoToken();
+    } else console.error("Kakao SDK가 로드되지 않았습니다.");
+  }, []);
+
+  const getKakaoToken = () => {
+    console.log(
+      window.Kakao.Auth.login({
+        success: (res) => console.log("성공"),
+      }),
+      "dd"
+    );
+    window.Kakao.Auth.login({
+      success: (res) => console.log("성공"),
+    });
+    Kakao.Auth.login({
+      success: (res) => {
+        console.log(res, "res");
+        loginState((state) => ({
+          ...state,
+          isLogin: true,
+          access_token: res.access_token,
+        }));
+      },
+      fail: (error) => {
+        console.error(error, "에러");
+      },
+    });
+
+    // if (cookie.refresh_token) {
+
+    // }
+  };
+
   return (
     <>
       {!globalValue.isLogin ? (
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,38 +4,61 @@ import API from "../utiles/api";
 import useInput from "../hooks/useInput";
 import { error } from "../utiles/toast";
 import userState from "../store/userState";
+
 import {
   useSetRecoilState, // 렌더링 불필요시 : getter & setter 방식 오브젝트 프로퍼티 get , set 후 값만 수정 (인풋같은거 사용)
   useResetRecoilState,
 } from "recoil";
-
+import axios from "axios";
+import { useCookies } from "react-cookie";
 const Login = () => {
-  const loginHandler = useSetRecoilState(userState); // 값만 변경 시키기
+  const loginState = useSetRecoilState(userState); // 값만 변경 시키기
   const resetState = useResetRecoilState(userState); // 디폴트값으로 값 변경
   const [value, changehandler] = useInput({
     email: "",
     password: "",
   });
-
+  const [cookie, setCookie] = useCookies(["name"]);
   const navigate = useNavigate();
   const isJoin = () => navigate("/join");
+  const expirationDate = new Date();
+  expirationDate.setMinutes(expirationDate.getMinutes() + 1);
+
+  const handleKakaoLogin = () => {
+    if (window.Kakao) {
+      Kakao.Auth.login({
+        success: (res) => {
+          console.log(res, "res");
+          console.log(res.access_token, "카카오 엑세스 토큰");
+          console.log(res.refresh_token, "카카오 리프레쉬 토큰");
+        
+          loginState((state) => ({
+            ...state,
+            isLogin: true,
+            access_token: res.access_token,
+          })); //전역변수에 엑세스 토큰 저장 로그인 상태 true 로 변경
+
+          setCookie("refresh_token", res.refresh_token, {
+            maxAge: res.refresh_token_expires_in,
+          }); // 리프레쉬 쿠키 설정
+
+          navigate("/");
+        },
+        fail: (error) => {
+          console.error(error, "에러");
+        },
+      });
+    }
+  };
   const isLogin = async () => {
     const res = await API.get("/users");
     try {
-      const emailTest = res.data.filter(
-        (current) => current.email === value.email
-      );
-      const pwTest = res.data.filter(
-        (current) => current.password === value.password
-      );
-      if (emailTest.length === 0) return error("아이디 틀려서 로그인못함");
-      if (pwTest.length === 0) return error("비번 틀려서 로그인못함");
-      loginHandler((state) => ({ ...state, isLogin: true, name: "ddd" }));
+      loginState((state) => ({ ...state, isLogin: true, name: "ddd" }));
       navigate("/");
     } catch (err) {
       console.log(err.response.data);
     } finally {
-      console.log(res.data);
+      console.log(res);
     }
   };
 
@@ -58,6 +81,7 @@ const Login = () => {
       <br />
 
       <button onClick={isLogin}>로그인</button>
+      <button onClick={handleKakaoLogin}>카카오 간편로그인</button>
       <p style={{ fontSize: "10px", cursor: "pointer" }} onClick={isJoin}>
         가입 페이지
       </p>
