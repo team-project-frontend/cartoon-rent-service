e7422a1400615941609c0e691dc33564a805b6f1
kmjang <rudals782@nate.com>
2023-05-18 01:53:37 +0900

    kmjang/feat/axios 훅

--- src/utiles/api.jsx
+++ src/utiles/api.jsx
@@ -3,9 +3,13 @@ import axios from "axios";
 const axiosConfig = {
   baseURL:
     process.env.NODE_ENV === "development"
-      ? `http://localhost:3000`
-      : `운영서버api`,
-  headers: { "Content-Type": "application/json" },
+      ? `https://test-xtcj6il6hq-du.a.run.app`
+      : `https://test-xtcj6il6hq-du.a.run.app`,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+
   withCredentials: true,
 };
 
